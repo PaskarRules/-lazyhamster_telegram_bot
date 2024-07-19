82dccbe45da75d54eeba8aaef512a5ca99d7317c
paska <lxpaskar@gmail.com>
2024-07-19 21:33:55 +0200

    optimize bot features;

--- bot.py
+++ bot.py
@@ -1,117 +1,158 @@
 import logging
 import requests
-import os
-
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, CallbackContext
-from dotenv import load_dotenv
 
-
-load_dotenv()
-
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+# Установка уровня логирования
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-TOKEN = os.getenv('YOUR_TELEGRAM_BOT_TOKEN')
+ANNOUNCEMENT_URL = 'http://127.0.0.1:8000/api/announcement/?limit={limit}&offset={offset}'
+CURRENT_ANNOUNCEMENT_URL = 'http://127.0.0.1:8000/api/announcement/{}/'
+ANNOUNCEMENT_COUNT_URL = 'http://127.0.0.1:8000/api/announcement/counter/'
+LIKE_URL = 'http://127.0.0.1:8000/api/announcement/{}/like/'
+DISLIKE_URL = 'http://127.0.0.1:8000/api/announcement/{}/dislike/'
 
-ANNOUNCEMENT_URL = 'http://139.59.215.111/api/announcement/'
-LIKE_URL = 'http://139.59.215.111/api/announcement/{}/like/'
-
-user_likes = {}
-last_post_id = {}
-active_chats = set()
+users_data = {}
 
 
 def start(update: Update, context: CallbackContext) -> None:
-    user_id = update.message.from_user.id
-    active_chats.add(user_id)
-    update.message.reply_text('Привет! Здесь вы можете следить за последними новостями. Новости обновляется на лету!')
+    user_id = update.effective_user.id
+
+    if user_id not in users_data:
+        users_data[user_id] = {'count': 0}
+
+    markups = [[InlineKeyboardButton('Получить новые сообщения', callback_data='poll')],]
+    new_markup = InlineKeyboardMarkup(markups)
+
+    update.message.reply_text(
+        text='Привет! Используйте кнопку снизу что бы всегда быть вкурсе!',
+        reply_markup=new_markup
+    )
 
 
-def get_posts(context: CallbackContext) -> None:
-    bot = context.bot
-    for user_id in active_chats:
-        response = requests.get(ANNOUNCEMENT_URL)
-        if response.status_code == 200:
-            posts = response.json()
-            last_id = last_post_id.get(user_id, 0)
-            new_posts = [post for post in posts if post["id"] > last_id]
-            if not new_posts:
-                continue
-
-            logger.info(f'Полученные новые данные для пользователя {user_id}: {new_posts}')
-            for post in new_posts:
-                title = post.get("title", "Без названия")
-                content = post.get("content", "Без содержания")
-                like_count = post.get("like_count", 0)
-                post_id = post["id"]
-                liked_by_user = user_likes.get(user_id, {}).get(post_id, False)
-                if liked_by_user:
-                    like_button_text = f"👍 {like_count} ❤️"
-                else:
-                    like_button_text = f"👍 {like_count}"
-                keyboard = [
-                    [InlineKeyboardButton(like_button_text, callback_data=f'like_{post_id}')]
-                ]
-                reply_markup = InlineKeyboardMarkup(keyboard)
-                bot.send_message(chat_id=user_id, text=f'{title}\n{content}', reply_markup=reply_markup)
-
-                last_post_id[user_id] = post_id
-        else:
-            bot.send_message(chat_id=user_id, text='Не удалось получить посты.')
-
-
-def button(update: Update, context: CallbackContext) -> None:
+def poll(update: Update, context: CallbackContext) -> None:
     query = update.callback_query
-    query.answer()
-    post_id = query.data.split('_')[1]
-    user_id = query.from_user.id
-    data = {
-        "user_id": user_id
-    }
-    response = requests.post(LIKE_URL.format(post_id), json=data)
+    response = requests.get(ANNOUNCEMENT_COUNT_URL)
+
     if response.status_code == 200:
-        post_response = requests.get(f'{ANNOUNCEMENT_URL}{post_id}')
-        if post_response.status_code == 200:
-            post = post_response.json()
-            title = post.get("title", "Без названия")
-            content = post.get("content", "Без содержания")
-            like_count = post.get("like_count", 0)
-            if user_likes.get(user_id, {}).get(post_id, False):
-                user_likes[user_id][post_id] = False
-                like_button_text = f"👍 {like_count}"
-            else:
-                if user_id not in user_likes:
-                    user_likes[user_id] = {}
-                user_likes[user_id][post_id] = True
-                like_button_text = f"👍 {like_count} ❤️"
-            keyboard = [
-                [InlineKeyboardButton(like_button_text, callback_data=f'like_{post_id}')]
-            ]
-            reply_markup = InlineKeyboardMarkup(keyboard)
-            query.edit_message_text(text=f'{title}\n{content}', reply_markup=reply_markup)
-        else:
-            query.edit_message_text(text='Не удалось получить обновленные данные поста.')
+        data = response.json()
+
+        count = data['count'] - users_data[query.from_user.id]['count']
+
+        if count == 0:
+            query.answer("Новых сообщений нету.", show_alert=True)
+            return
+
+        users_data[query.from_user.id]['limit'] = 1
+        query.answer(f"Новых сообщений {count}. Изучайте с самих свежих новостей!", show_alert=True)
+        poll_announcement(update, context)
     else:
-        query.edit_message_text(text='Не удалось лайкнуть пост.')
+        query.answer('Ошибка при выполнении запроса', show_alert=True)
 
 
-def periodic_task(context: CallbackContext) -> None:
-    get_posts(context)
+def poll_announcement(update: Update, context: CallbackContext, url='') -> None:
+    query = update.callback_query
+    user_id = query.from_user.id
+
+    limit = users_data[query.from_user.id]['limit']
+    offset = users_data[query.from_user.id]['count']
+
+    response = requests.get(ANNOUNCEMENT_URL.format(limit=limit, offset=offset) if not url else url)
+    if response.status_code == 200:
+        data = response.json()
+
+        users_data[user_id]['count'] = data['count']
+        users_data[user_id]['posts'] = data['results']
+        users_data[user_id]['next'] = data['next']
+        users_data[user_id]['previous'] = data['previous']
+    else:
+        query.answer('Ошибка при выполнении запроса', show_alert=True)
+
+    show_posts(update, context)
+
+
+def show_posts(update: Update, context: CallbackContext) -> None:
+    query = update.callback_query
+
+    user_id = query.from_user.id
+    post = users_data[user_id]['posts'][0]
+
+    keyboard = [[
+        InlineKeyboardButton(f"❤️ {post['like_count']}", callback_data=f'like_{post["id"]}'),
+        InlineKeyboardButton(f"💔 {post['dislike_count']}", callback_data=f'dislike_{post["id"]}')
+    ]]
+    prev_next_b = []
+    if users_data[user_id]['previous']:
+        prev_next_b.append(InlineKeyboardButton("Назад", callback_data='prev_page'))
+
+    if users_data[user_id]['next']:
+        prev_next_b.append(InlineKeyboardButton("Вперед", callback_data='next_page'))
+
+    keyboard.append(prev_next_b)
+    keyboard.append([InlineKeyboardButton('Получить новые сообщения', callback_data='poll')])
+
+    new_text = f"{post['title']}\n{post['description']}"
+    new_markup = InlineKeyboardMarkup(keyboard)
+
+    try:
+        query.edit_message_text(
+            text=new_text,
+            reply_markup=new_markup
+        )
+    except Exception as e:
+        logger.error(f"Failed to edit message: {e}")
+
+
+def change_page(update: Update, context: CallbackContext) -> None:
+    query = update.callback_query
+    user_id = query.from_user.id
+    data = query.data
+    url = ''
+
+    if data == 'next_page':
+        url = users_data[user_id]['next']
+    elif data == 'prev_page':
+        url = users_data[user_id]['previous']
+
+    poll_announcement(update, context, url=url)
+
+
+def like_dislike(update: Update, context: CallbackContext) -> None:
+    query = update.callback_query
+    user_id = query.from_user.id
+    data = query.data
+    action, post_id = data.split('_')
+    post_id = int(post_id)
+
+    if action == 'like':
+        url = LIKE_URL.format(post_id)
+    else:
+        url = DISLIKE_URL.format(post_id)
+
+    response = requests.post(url, json={'user_id': user_id})
+    if response.status_code == 200:
+        announcement_response = requests.get(CURRENT_ANNOUNCEMENT_URL.format(post_id))
+        if announcement_response.status_code == 200:
+            announcement_data = announcement_response.json()
+            users_data[user_id]['posts'] = [announcement_data]
+
+            show_posts(update, context)
+
+    else:
+        query.answer('Ошибка при выполнении запроса', show_alert=True)
 
 
 def main() -> None:
-    if not TOKEN:
-        raise ValueError("Необходимо задать переменную окружения 'YOUR_TELEGRAM_BOT_TOKEN'")
+    updater = Updater("7441871392:AAHlh0VLEhtIUmBTx1-FMAT9JgFLlp_Lp2s")
 
-    updater = Updater(token=TOKEN, use_context=True)
     dispatcher = updater.dispatcher
 
     dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CallbackQueryHandler(button))
-
-    job_queue = updater.job_queue
-    job_queue.run_repeating(periodic_task, interval=10, first=0)
+    dispatcher.add_handler(CallbackQueryHandler(poll, pattern='^poll$'))
+    dispatcher.add_handler(CallbackQueryHandler(show_posts, pattern='^show_posts$'))
+    dispatcher.add_handler(CallbackQueryHandler(change_page, pattern='^(next_page|prev_page|first_page|last_page)$'))
+    dispatcher.add_handler(CallbackQueryHandler(like_dislike, pattern='^(like|dislike)_'))
 
     updater.start_polling()
     updater.idle()
